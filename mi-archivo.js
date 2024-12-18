1112ea4f094aeefbd90b1feb56c10115059b233b
gasome27 <63860816+gasome27@users.noreply.github.com>
2024-12-17 23:42:44 -0600

    Update mi-archivo.js

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,1 +1,34 @@
+console.log("Hola Mundo_editado");
+console.log("cambios en el archivo");
 
+git clone <URL-del-repositorio>
+
+  npm install fullcalendar
+  
+<link href="https://cdm.jsdelivr.net/npm/fullcalendar@6.18/index.global.css" rel="stylesheet">
+  
+  <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js"></script>
+
+<div id="calendar></div>
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  var calendarEl = document.getElementById('calendar');
+  
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      
+      initialView: 'dayGridMonth'
+      
+    });
+  
+  calendar.render();
+  
+});
+
+// Issues
+
+// https://github.com/tlgadrkn/full-calendar-example
+
+// https://github.com/fullcalendar/fullcalendar/issues/7845#issue-2745829248
+
+// mi gist <script src="https://gist.github.com/gasome27/659172fce8afe36bc8787b7202279edc.js"></script>
